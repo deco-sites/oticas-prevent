2b47a68f23a094ecfb73d5ee1c6dcc5e235c1667
Italo Barreto <italo@agenciafg.com.br>
2024-06-06 15:03:20 -0300

    modal mapa nossas lojas

--- components/ui/Lojas.tsx
+++ components/ui/Lojas.tsx
@@ -1,6 +1,9 @@
 import type { ImageWidget } from "apps/admin/widgets.ts";
 import Image from "apps/website/components/Image.tsx";
-
+import { useUI } from "$store/sdk/useUI.ts";
+import ModalCustom from "$store/components/ui/ModalCustom.tsx";
+import { computed, signal } from "@preact/signals";
+import { useCallback, useEffect, useState } from "preact/hooks";
 export interface Props {
   imagem?: {
     Imagem: ImageWidget;
@@ -24,6 +27,10 @@ export interface Props {
    */
   horario?: string;
   /**
+   * @title Localização
+   */
+  mapa?: string;
+  /**
    * @title Imagem à direita?
    */
   lado?: boolean;
@@ -36,9 +43,21 @@ export default function Lojas({
   horario,
   email,
   imagem,
+  mapa,
   lado,
 }: Props) {
   const ladoimg = lado === true ? "direito" : "esquerdo";
+
+  const [displayMapsModal, setDisplayMapsModal] = useState(false);
+
+  const openModalMaps = () => {
+    setDisplayMapsModal(true);
+  };
+
+  const closeModalMaps = () => {
+    setDisplayMapsModal(false);
+  };
+
   return (
     <div class={`loja grid grid-cols-1 lg:grid-cols-2 ${ladoimg}`}>
       <div
@@ -93,6 +112,75 @@ export default function Lojas({
           {horario && horario.trim() !== "" && (
             <p class="my-2 w-full">{horario}</p>
           )}
+
+          {mapa && <p class="btn" onClick={openModalMaps}>Ver no mapa</p>}
+          {displayMapsModal == true
+            ? (
+              <ModalCustom
+                loading="lazy"
+                open={displayMapsModal}
+                onClose={() => setDisplayMapsModal(false)}
+              >
+                <div class="w-screen h-screen flex">
+                  <div class="flex flex-col gap-3 md:gap-2 bg-white justify-start m-auto relative rounded-lg w-11/12 md:w-auto">
+                    <div class="relative px-6 py-6 bg-white rounded-lg">
+                      <div
+                        class="absolute no-animation right-3 top-3 rounded-full border-2 border-[#119184] cursor-pointer p-1"
+                        onClick={closeModalMaps}
+                      >
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          width="17"
+                          height="17"
+                          viewBox="0 0 17 17"
+                          fill="none"
+                        >
+                          <g clip-path="url(#clip0_144_3780)">
+                            <path
+                              d="M13 4.20459L4 13.2046"
+                              stroke="#119184"
+                              stroke-width="2"
+                              stroke-linecap="round"
+                              stroke-linejoin="round"
+                            />
+                            <path
+                              d="M13 13.2046L4 4.20459"
+                              stroke="#119184"
+                              stroke-width="2"
+                              stroke-linecap="round"
+                              stroke-linejoin="round"
+                            />
+                          </g>
+                          <defs>
+                            <clipPath id="clip0_144_3780">
+                              <rect
+                                width="16"
+                                height="16"
+                                fill="white"
+                                transform="translate(0.5 0.70459)"
+                              />
+                            </clipPath>
+                          </defs>
+                        </svg>
+                      </div>
+                      {tituloloja && (
+                        <h2 class="font-semibold mb-4 text-xl text-[#5f9b9a] w-full pr-4">
+                          {tituloloja}
+                        </h2>
+                      )}
+                      {mapa && (
+                        <div
+                          class="mapa-container"
+                          dangerouslySetInnerHTML={{ __html: mapa }}
+                        >
+                        </div>
+                      )}
+                    </div>
+                  </div>
+                </div>
+              </ModalCustom>
+            )
+            : null}
         </div>
       </div>
     </div>
